ecc2bf04f3ef0b7fff491758c379294740ecdf08
gl-harolds <harolds@glsolutions.com>
2025-07-31 22:40:41 +0000

    API setup

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-43rlip4za_kwj*z34xphm))ya&8pd&$t5v&f=_210*qxexa6^$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'scaling-broccoli-97xgxrqgqrx6fxv6g-8000.app.github.dev']
 
 
 # Application definition
